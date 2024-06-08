7c50aff32dc94f4ac08cd30744621beee89b1fc0
denkeleo <denke@stud.uniza.sk>
2024-06-08 15:13:14 +0200

    Testing if small changes in code commit a push to cloud correctly.

--- app/src/main/java/com/example/mymusic/MainActivity.kt
+++ app/src/main/java/com/example/mymusic/MainActivity.kt
@@ -19,7 +19,7 @@ class MainActivity : ComponentActivity() {
             MyMusicTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+                    Greeting("User")
                 }
             }
         }
@@ -29,7 +29,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-            text = "Hello $name!",
+            text = "Hello $name!\nWelcome to MyMusic",
             modifier = modifier
     )
 }
@@ -38,6 +38,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     MyMusicTheme {
-        Greeting("Android")
+        Greeting("User")
     }
 }
